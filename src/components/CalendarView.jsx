630bb322565b68a751542d2082e4bd578aed79e6
Tsumari <lucasflicky@hotmail.com>
2022-07-27 16:10:44 +0200

    move currency manager one level above (to calendarview)

--- src/components/CalendarView.jsx
+++ src/components/CalendarView.jsx
@@ -1,27 +1,45 @@
-import {useState} from 'react';
+import {useState,useRef,useEffect} from 'react';
 import { LocaleProvider } from "../contexts/LocaleContext";
 
 import MonthView from './MonthView';
 import CalendarMonth from './CalendarMonth';
 
+import getRates from '../data/currencyRates';
+
+
 export default function CalendarView({year}){
   
     const monthsRange =  Object.keys([...new Array(12)]).map(x=>~~x);
-    console.log(year);
     year ??= new Date().getFullYear();
-    console.log(year,'b');
     
     const [locale,setLocale] = useState();
     const [selectedMonth,selectMonth] = useState(0);
+
+
+    const [currRates, setRates] = useState({loading:true});	
+    const [baseCurrency, setBaseCurrency] = useState("PLN");
+    const baseCurrencyRef = useRef();
+	  const updateCurrencyRates = (base) => getRates(base).then( rates => setRates({loading:false,rates}) );
   
+    useEffect(() => {
+      setTimeout(()=>{
+        updateCurrencyRates(baseCurrency)
+      },0) // testing async
+    }, [baseCurrency]);
+
     return (
       <>
+        <select onChange={()=>setBaseCurrency(baseCurrencyRef.current.value)} ref={baseCurrencyRef}>
+          <option value="PLN">PLN</option>
+          <option value="USD">USD</option>
+          <option value="BRL">BRL</option>
+        </select>
         <div className="calendar-body flex-wrap max-w-xl p-3">
           {
             monthsRange.map((month,i)=>{
               return (
                 <LocaleProvider.Provider value={{locale,setLocale}} key={i}>
-                  <CalendarMonth month={month} year={year} selectMonth={selectMonth} />
+                  <CalendarMonth baseCurrency={baseCurrency} currRates={currRates} month={month} year={year} selectMonth={selectMonth} />
                 </LocaleProvider.Provider>
               )
             })
