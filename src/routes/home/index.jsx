5d12cb2ac040bff267966e5415393f6fee8c1b58
Tsumari <lucasflicky@hotmail.com>
2022-07-27 16:10:21 +0200

    move index

---
+++ src/routes/home/index.jsx
@@ -0,0 +1,18 @@
+import './main.css';
+import CalendarView from '../../components/CalendarView';
+
+function Home() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header"> </header>
+      <section className="container-main">
+        test
+        <CalendarView />        
+      </section>
+    </div>
+  );
+}
+
+export default Home;
