3d8456e30019ed35f5f5af352969577b029a6d9c
Tsumari <lucasflicky@hotmail.com>
2022-07-27 14:50:03 +0200

    digest currency converter

--- src/components/CalendarMonth.jsx
+++ src/components/CalendarMonth.jsx
@@ -2,16 +2,13 @@ import {useState,useContext,useEffect} from 'react';
 import {query,collection,onSnapshot} from 'firebase/firestore';
 import {db} from '../data/firestore';
 import { LocaleProvider } from "../contexts/LocaleContext";
+import getRates from '../data/currencyRates';
 
-
-const MonthTools = {
-	reducer: (p, c) => p + c.value || 0,
-	filterByType: (type) => (itm) => itm.confirmed && itm.type === type,
-};
 export default function CalendarMonth({ month, selectMonth }) {
 	
 	const { locale } = useContext(LocaleProvider);
 	const [entries, setEntries] = useState([]);
+	const [currRates, setRates] = useState({loading:true});
 
 	useEffect(() => {
 		const q = query(collection(db, "calendata"));
@@ -22,21 +19,32 @@ export default function CalendarMonth({ month, selectMonth }) {
 			});
 			setEntries(dbItems);
 		});
+
+		setTimeout(()=>{
+			getRates("PLN").then( rates => setRates({loading:false,rates}) )
+		},0) // testing async
+
 		return () => unsub();
 	}, []);
+
 
 	const monthName = new Date(1, month, 1).toLocaleString(locale, {
 		month: "long",
 	});
 	const entriesFromThisMonth = entries.filter((entry) => entry.month === month);
 
-	const month_total_in = entriesFromThisMonth
-		.filter(MonthTools.filterByType("income"))
-		.reduce(MonthTools.reducer, 0);
 
-	const month_total_out = entriesFromThisMonth
-		.filter(MonthTools.filterByType("expense"))
-		.reduce(MonthTools.reducer, 0);
+	const reducer= (p, c) => p + (c.value || 0)  * currRates.rates?.[c.currency];
+	const filterByType= (type) => (itm) => itm.confirmed && itm.type === type;
+
+
+	const month_total_in = ~~ entriesFromThisMonth
+		.filter(filterByType("income"))
+		.reduce(reducer, 0);
+
+	const month_total_out = ~~ entriesFromThisMonth
+		.filter(filterByType("expense"))
+		.reduce(reducer, 0);
 
 	const net_total = month_total_in - month_total_out;
 
@@ -52,24 +60,31 @@ export default function CalendarMonth({ month, selectMonth }) {
 					{" "}
 					{monthName}{" "}
 				</div>
-				<div className="month-body px-3 py-1">
-					<div className="justify-between flex">
-						<span className="font-light text-slate-700">Income:</span>
-						<span className="text-green-600">{month_total_in}</span>
+				{ currRates.loading 
+					? 
+						<> <div> Loading... </div> </> 
+					:						
+						<div className="month-body px-3 py-1">
+						<div className="justify-between flex">
+							<span className="font-light text-slate-700">Income:</span>
+							<span className="text-green-600">{month_total_in}</span>
+						</div>
+						<div className="justify-between flex">
+							<span className="font-light text-slate-700">Expenses:</span>
+							<span className="text-red-600">{month_total_out}</span>
+						</div>
+						<div className="justify-between flex">
+							<span className="font-light text-slate-700">Net Total:</span>
+							<span
+								className={net_total > 0 ? "text-emerald-700" : "text-red-700"}
+							>
+								{net_total}
+							</span>
+						</div>
 					</div>
-					<div className="justify-between flex">
-						<span className="font-light text-slate-700">Expenses:</span>
-						<span className="text-red-600">{month_total_out}</span>
-					</div>
-					<div className="justify-between flex">
-						<span className="font-light text-slate-700">Net Total:</span>
-						<span
-							className={net_total > 0 ? "text-emerald-700" : "text-red-700"}
-						>
-							{net_total}
-						</span>
-					</div>
-				</div>
+					
+				}
+				
 			</div>
 		</>
 	);
