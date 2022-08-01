e8ceb4762535c24f2a26d28d9c671d92a2ea26ba
Tsumari <lucasflicky@hotmail.com>
2022-08-01 14:35:09 +0200

    move locale selector to parent

--- src/components/Calendar/CalendarView.jsx
+++ src/components/Calendar/CalendarView.jsx
@@ -3,23 +3,12 @@ import {useState} from 'react';
 import MonthView from './MonthView';
 import CalendarMonth from './CalendarMonth';
 
-import { LocaleProvider } from '../../contexts/LocaleContext';
 
-export default function CalendarView({year}){
-  
-    const monthsRange =  Object.keys([...new Array(12)]).map(x=>~~x);
-    year ??= new Date().getFullYear();
-
-    
-    const [locale,setLocale] = useState();
-    const [selectedMonth,selectMonth] = useState(0);
- 
-    
-  
+export default function CalendarView({year,selectedMonth,selectMonth,monthsRange}){
 
     return (
       <>        
-        <LocaleProvider.Provider value={{locale,setLocale}}>
+        
           <div className="calendar-body flex-wrap max-w-xl p-3">
             {
               monthsRange.map((month,i)=>  
@@ -30,7 +19,7 @@ export default function CalendarView({year}){
           <div className="month-details-container">
             <MonthView month={selectedMonth||0} year={year} />
           </div>
-        </LocaleProvider.Provider>
+        
       </>
     )
     
--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -3,19 +3,24 @@ import CalendarView from '../../components/Calendar/CalendarView';
 import Wishlist from '../../components/Wishlist/WishlistMain';
 import { useState, useEffect } from 'react';
 import CurrencyContext from '../../contexts/CurrencyContext';
-import CurrencyPicker from '../../components/BasicUI/CurrencyPicker';
-
+import CurrencyPicker from '../../components/BasicUI/CurrencyPicker';  
+import { LocaleProvider } from '../../contexts/LocaleContext';
+    
 import getRates from '../../data/currencyRates';
+import Dropdown from '../../components/BasicUI/Dropdown';
 
 function Home() {
 
   const [screen,setScreen] = useState("WISHLIST");
-  
 
-
-
+  const [locale,setLocale] = useState();
   const [baseCurrency, setBaseCurrency] = useState("PLN");
   const [currencyRates, setRates] = useState({loading:true});
+
+  
+  const [selectedMonth,selectMonth] = useState(0);
+  const monthsRange =  Object.keys([...new Array(12)]).map(x=>~~x);
+  const [year,setYear] = useState(new Date().getFullYear());
 
   
   const updateCurrencyRates = (base) => getRates(base).then( rates => setRates({loading:false,rates}) );
@@ -28,20 +33,26 @@ function Home() {
 
   return (
     <CurrencyContext.Provider value={{currencyRates,setRates,baseCurrency,setBaseCurrency}}>
+      <LocaleProvider.Provider value={{locale,setLocale}}>
       
-    <div className="App">
-      <header className="App-header"> 
-      
-      <CurrencyPicker onChange={ setBaseCurrency }  placeholder="Select base currency..." />
-      
-      </header>
-      <section className="container-main">
-              
-        <CalendarView />        
-        <Wishlist />        
-      </section>
-    </div>
+        
+        <div className="App">
+          <header className="App-header"> 
+          
+          <CurrencyPicker onChange={ setBaseCurrency }  placeholder="Select base currency..." />
+          <Dropdown onChange={selectMonth} options={ monthsRange.map(m => ({ value:m, label: new Date(1,m,1).toLocaleString(locale,{month:"long"}) }))}/>          
+          </header>
+          <section className="container-main">
+                  
+            <CalendarView { ...{selectedMonth,selectMonth,monthsRange,year} }/>
+            <Wishlist />
 
+            
+                    
+          </section>
+        </div>
+
+      </LocaleProvider.Provider>
     </CurrencyContext.Provider>
   );
 }
