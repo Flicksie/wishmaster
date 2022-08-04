3ff458600fde99bfafb244f7b1f6a3cd1b7fb764
Tsumari <lucasflicky@hotmail.com>
2022-08-04 21:38:58 +0200

    create UI utilities

---
+++ src/components/BasicUI/utils.js
@@ -0,0 +1,13 @@
+export const PriorityColorScale = [
+    "#F58282",
+    "#F49783",
+    "#F4AD84",
+    "#F3C284",
+    "#F2D785",
+    "#D6D7A1",
+    "#BAD7BD",
+    "#9ED6D9",
+    "#76c8c8",
+    "#54bebe",
+    "#349ebe",
+];
