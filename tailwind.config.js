1d1efccec8e532b1539e81760b92721d0c0a8278
Tsumari <lucasflicky@hotmail.com>
2022-08-07 14:01:35 +0200

    add fade and slide animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        fadeIn: "fade-in .5s ease forwards",
+        fadeOut: "fade-in .5s ease reverse",
+        slideRight: "slide-right .5s ease forwards",
+      },
+
+      keyframes:{
+        "fade-in": {
+          "0%": {opacity:0},
+          "100%": {opacity:1}
+        },
+        "slide-right": {
+          "0%": {transform: "translateX(-100%)" },
+          "100%": {transform:" translateX(0)" }          
+        }
+      }
+    },
   },
   plugins: [],
 }
